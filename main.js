a889902d077c1923ce61a2ffd641e789e445d528
“luizmendesgit config --global user.email “luizfelipemd10@hotmail.com <luizfelipemd10@hotmail.com>
2023-05-17 21:58:54 -0300

    final adjustments

--- main.js
+++ main.js
@@ -1,7 +1,42 @@
-const randomNumber = Math.round(Math.random() * 10)
+const cookieImage = document.querySelector("#cookie-image");
+const openAnotherBtn = document.querySelector(".open-button");
+const sectionTitle = document.querySelector("#fortune-cookie h1");
+const sectionText = document.querySelector("#fortune-cookie p");
+const luckText = document.querySelector("#luck-container");
 
-btnCokiee.addEventListener("click", handleTryClick)
-btnOpen.addEventListener("click", function () {
-  document.querySelector(".screen1").classList.remove("hide")
-  document.querySelector(".screen2").classList.add("hide")
-})
+let luckPhrases = [
+  "A vida trará coisas boas se tiver paciência.",
+  "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
+  "Todas as coisas são difícieis antes de se tornarem fáceis.",
+  "Lamentar aquilo que não temos é desperdiçar aquilo que já possuímos.",
+];
+
+cookieImage.addEventListener("click", openCookie);
+openAnotherBtn.addEventListener("click", openAnotherCookie);
+
+function openCookie() {
+  sectionTitle.innerText = "Aqui está a sua sorte de hoje:";
+  cookieImage.setAttribute("src", "./assets/opened-cookie.png");
+
+  loadRandomLuckPhrase();
+  resetTexts();
+}
+
+function openAnotherCookie() {
+  sectionTitle.innerText = "Qual é a sua sorte de hoje?";
+  cookieImage.setAttribute("src", "./assets/fortune-cookie.png");
+
+  resetTexts();
+}
+
+function resetTexts() {
+  cookieImage.classList.toggle("closed");
+  luckText.classList.toggle("hide");
+  sectionText.classList.toggle("hide");
+  openAnotherBtn.classList.toggle("hide");
+}
+
+function loadRandomLuckPhrase() {
+  let randomIndex = Math.floor(Math.random() * luckPhrases.length);
+  luckText.innerHTML = luckPhrases[randomIndex];
+}
